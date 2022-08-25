be383d66e406f11b2a121585de1c1bce756a5811
Wael Mohammed <wmamohammed1@sheffield.ac.uk>
2022-08-25 07:32:06 +0100

    added dockerfiles for docker-based Repast-HPC

---
+++ creating-a-new-alpine-mpich-image/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# adapted from https://github.com/NLKNguyen/alpine-mpich/blob/master/base/mpich-test/test.sh
+set -e
+
+echo "--- Test MPICH installation ---"
+
+printf "it should find mpicc... "
+mpicc --version > /dev/null
+echo ok
+
+printf "it should find mpiexec... "
+mpiexec --version > /dev/null
+echo ok
+
+printf "it should compile mpi_hello_world.c source... "
+mpicc -o mpi_hello_world mpi_hello_world.c > /dev/null
+echo ok
+
+printf "it should run mpi_hello_world program successfully... "
+mpirun -n 4 ./mpi_hello_world > /dev/null
+echo ok
